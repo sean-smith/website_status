fd3f4daebda8f4546825c102e4e03c6c13e79af7
Sean Smith <swsmith@bu.edu>
2014-12-11 02:39:04 -0500

    added yo capability when down

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-website_status
+Website Status
 ==============
 
-Python script to check if website is up (meant to be run with a cronjob)
+This python script will check if your page is online and send an email if it's not. To set up first clone the repo and change the email address and url to your url and then make a cronjob to run it as often as you'd like.
+
+To Set up:
+----------
+
+    $ git clone https://github.com/sean-smith/website_status.git
+    $ crontab -t 
--- web_status.py
+++ web_status.py
@@ -1,6 +1,6 @@
+#!/usr/bin/python
 import requests
 import smtplib
-import time
 
 def is_website_working(url, string_in_page):
     try:
@@ -9,19 +9,46 @@ def is_website_working(url, string_in_page):
             return False
         return True
     except:
+        send_mail(url)
         return False
 
 def send_mail(url):
-    s = smtplib.SMTP('localhost')
-    s.sendmail("seanwssmith@gmail.com", ["seanwssmith@gmail.com"], "WEBSITE DOWN"+ url)
-    s.quit()
+    sender = 'seanwssmith@gmail.com'
+    receivers = ['seanwssmith@gmail.com']
+
+    message = """From: From Person <seanwssmith@gmail.com>
+    To: To Person <seanwssmith@gmail.com>
+    Subject: WEBSITE MAIL
+
+    Your website is down!.
+    """
+
+    try:
+       smtpObj = smtplib.SMTP('localhost')
+       smtpObj.sendmail(sender, receivers, message)
+       print "Successfully sent email"
+    except:
+       print "Error: unable to send email"
+
+
+
+def send_yo(username, url):
+    data={'api_token': "048fd446-d462-4524-93c0-ad644d3387ee", 'username': username}
+    data["link"] = url
+    r = requests.post("http://api.justyo.co/yo/", data)
+    return r.status_code
+
+
+
+
+
 
 def main():
-    
+    return 2
 
 
 
 
 
-print is_website_working("http://54.148.80.59:82/", "Sean")
+print is_website_working("http://seanssmith.me/", "Sean")
 send_mail("url")
