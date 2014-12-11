73f290ea1ac4f0eff25e8b787c017581f6a97dee
Sean Smith <swsmith@bu.edu>
2014-12-11 04:28:46 -0500

    removed personal data :)

--- website.sh
+++ website.sh
@@ -1,2 +1,9 @@
-#!/bin/bash          
-wget("http://54.148.80.59:82/") 
+#!/bin/bash
+#this is only if every minute isn't fast enough for you :)
+#This runs every 5 seconds. Just change the path and run it in the background
+# ./website.sh & <== To run in background
+while True
+do
+    python /path/to/file
+    sleep 5
+done
